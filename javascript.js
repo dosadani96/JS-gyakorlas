64f1eb038d4aa1dc570f4e51b03228931cd310a3
dosadani96 <63305205+dosadani96@users.noreply.github.com>
2020-04-10 16:08:49 +0200

    added

---
+++ javascript.js
@@ -0,0 +1,18 @@
+console.log("megy");
+
+
+$("btn").on('click', (event) => {
+    console.log("Yeah, you clicked me");
+  });
+  
+  $("#second").click(() => {
+    $("#first").text("Second one was clicked.");
+  });
+  
+  $("#prettify").click(() => {
+    $("button").css("background", "purple");
+    $("button").css("color", "white");
+    $("button").css("font-size", "20px");
+    $("button").css("border", "0");
+    $("button").css("border-radius", "5px");
+  })
